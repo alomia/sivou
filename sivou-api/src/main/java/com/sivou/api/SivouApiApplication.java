9bea74069755a5e0bec569d6a59e5f6dd17d8863
Alejandro Alomia <alejandroalomia730@gmail.com>
2026-04-12 02:22:25 -0500

    chore: configuración inicial del proyecto con Spring Boot

---
+++ sivou-api/src/main/java/com/sivou/api/SivouApiApplication.java
@@ -0,0 +1,13 @@
+package com.sivou.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivouApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivouApiApplication.class, args);
+	}
+
+}
